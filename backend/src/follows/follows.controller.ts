418629fcfa8ed5ac30249acada4590ff87331ef8
cuonggpham <qcuonghls@gmail.com>
2025-08-01 21:42:33 +0700

    feat: add unfollow function and get number of follower
    a bit

--- backend/src/follows/follows.controller.ts
+++ backend/src/follows/follows.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Param } from '@nestjs/common';
+import { Controller, Post, Param, Get } from '@nestjs/common';
 import { FollowsService } from './follows.service';
 import { CurrentUser } from '@common/decorators/user.decorator';
 import { FollowResponseDto } from './dtos/follow.response.dto';
@@ -9,6 +9,14 @@ import { Auth } from '@common/decorators/auth.decorator';
 export class FollowsController {
   constructor(private readonly followsService: FollowsService) {}
 
+  @Get(':sellerId/isFollowing')
+  @Auth(AuthType.ACCESS_TOKEN)
+  async getFollowStatus(
+    @Param('sellerId') sellerId: string,
+  ): Promise<{ followerCount: number }> {
+    return this.followsService.getFollowNumber(sellerId);
+  }
+
   @Post(':sellerId/follow')
   @Auth(AuthType.ACCESS_TOKEN)
   async followSeller(
@@ -16,5 +24,14 @@ export class FollowsController {
     @CurrentUser() currentUser: { id: string; email: string },
   ): Promise<FollowResponseDto> {
     return this.followsService.followSeller(sellerId, currentUser);
+  }
+
+  @Post(':sellerId/unfollow')
+  @Auth(AuthType.ACCESS_TOKEN)
+  async unfollowSeller(
+    @Param('sellerId') sellerId: string,
+    @CurrentUser() currentUser: { id: string; email: string },
+  ): Promise<{ message: string }> {
+    return this.followsService.unfollowSeller(sellerId, currentUser);
   }
 }
